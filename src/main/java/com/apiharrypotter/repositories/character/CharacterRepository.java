2203b2e67fe240bbc9e8c836967ae8dba6b1a1d5
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-11-28 20:51:08 -0300

    Modified parameter

--- src/main/java/com/apiharrypotter/repositories/character/CharacterRepository.java
+++ src/main/java/com/apiharrypotter/repositories/character/CharacterRepository.java
@@ -1,8 +1,9 @@
 package com.apiharrypotter.repositories.character;
 
+import com.apiharrypotter.entities.Characters;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CharacterRepository extends JpaRepository<Character, Long> {
+public interface CharacterRepository extends JpaRepository<Characters, Long> {
 }
