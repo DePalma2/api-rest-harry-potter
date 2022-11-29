6c9540efe9fc368dd8f6fa8121b4e2ef299a9a5f
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-11-28 21:17:16 -0300

    Create Potion service

---
+++ src/main/java/com/apiharrypotter/services/potion/PotionService.java
@@ -0,0 +1,15 @@
+package com.apiharrypotter.services.potion;
+
+import com.apiharrypotter.entities.Potion;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PotionService {
+
+    public List<Potion> getAllPotions(Potion potion);
+    public Optional<Potion> getById(Long id);
+    public Potion addPotion(Potion potion);
+    public Potion updatePotion(Long id, Potion potion);
+    public boolean deletePotion(Potion potion);
+}
