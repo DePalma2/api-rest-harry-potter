2fd691d6facefb92d638887873ceec9f912c6cf1
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-12-04 20:52:37 -0300

    Cross Origin

--- src/main/java/com/apiharrypotter/ApiHarryPotterApplication.java
+++ src/main/java/com/apiharrypotter/ApiHarryPotterApplication.java
@@ -2,8 +2,11 @@ package com.apiharrypotter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 @SpringBootApplication
+@CrossOrigin(origins = "*")
 public class ApiHarryPotterApplication {
 
     public static void main(String[] args) {
