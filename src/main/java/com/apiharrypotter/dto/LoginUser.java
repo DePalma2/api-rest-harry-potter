bea041c64d11bc2e65f0a50314f384bc05f173bc
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-12-06 14:38:01 -0300

    feat(DTO): New DTOs
    LoginUser, NewUser

---
+++ src/main/java/com/apiharrypotter/dto/LoginUser.java
@@ -0,0 +1,27 @@
+package com.apiharrypotter.dto;
+
+import javax.validation.constraints.NotBlank;
+
+public class LoginUser {
+
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
