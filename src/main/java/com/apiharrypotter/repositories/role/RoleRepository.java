036f6e8120171154bc206ca7986bcfc541124998
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-12-06 14:33:21 -0300

    feat(Repository): New Repositories
    Add RoleRepository & UserRepository

--- src/main/java/com/apiharrypotter/repositories/role/RoleRepository.java
+++ src/main/java/com/apiharrypotter/repositories/role/RoleRepository.java
@@ -1,2 +1,13 @@
-package com.apiharrypotter.repositories.role;public class RoleRepository {
+package com.apiharrypotter.repositories.role;
+
+import com.apiharrypotter.entities.Rol;
+import com.apiharrypotter.enums.ERoles;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Rol, Long> {
+    Optional<Rol> findByRolName(ERoles roleName);
 }
---
+++ src/main/java/com/apiharrypotter/repositories/user/UserRepository.java
@@ -0,0 +1,15 @@
+package com.apiharrypotter.repositories.user;
+
+import com.apiharrypotter.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername (String username);
+    boolean existsByEmail (String email);
+
+}
