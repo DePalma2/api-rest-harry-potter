67cb2e30f5e9bbd32859b3d36bac91f44e9614cb
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-12-06 14:37:29 -0300

    feat(Config): UserDetails

---
+++ src/main/java/com/apiharrypotter/config/CustomUserDetails.java
@@ -0,0 +1,101 @@
+package com.apiharrypotter.config;
+
+import com.apiharrypotter.entities.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class CustomUserDetails implements UserDetails {
+
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(String name, String username, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public static CustomUserDetails build(User usuario){
+        List<GrantedAuthority> authorities =
+                usuario.getRoles()
+                        .stream()
+                        .map(rol -> new SimpleGrantedAuthority(rol.getRoleName().name()))
+                        .collect(Collectors.toList());
+        return new CustomUserDetails(usuario.getName(), usuario.getUsername(), usuario.getEmail(),
+                usuario.getPassword(), authorities);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
