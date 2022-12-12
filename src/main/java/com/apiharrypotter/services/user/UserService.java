0993e60c06e612665e0ef86da861c723f09a363c
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-12-12 17:16:01 -0300

    build(service): User

--- src/main/java/com/apiharrypotter/services/user/UserService.java
+++ src/main/java/com/apiharrypotter/services/user/UserService.java
@@ -1,31 +1,37 @@
 package com.apiharrypotter.services.user;
 
-import com.apiharrypotter.entities.User;
+import com.apiharrypotter.config.jwt.JwtUsers;
 import com.apiharrypotter.repositories.user.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
 @Service
-@Transactional
+@RequiredArgsConstructor
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    public Optional<User> getByUsername(String username){
-        return userRepository.findByUsername(username);
+    public JwtUsers save(JwtUsers users) {
+        return userRepository.save(users);
     }
-    public Boolean existsByUsername(String username){
-        return userRepository.existsByUsername(username);
+
+    public Optional<JwtUsers> findUserByEmail(String email) {
+        return userRepository.findJwtUsersByEmail(email);
     }
-    public Boolean existsByEmail(String email){
-        return userRepository.existsByEmail(email);
+
+    public JwtUsers getUserByEmail(String email) {
+        return userRepository
+                .findJwtUsersByEmail(email)
+                .orElseThrow(() -> new EntityNotFoundException("User Not found by email " + email));
     }
-    public void save(User username){
-        userRepository.save(username);
+
+    public JwtUsers getUserByUsername(String username) {
+        return userRepository
+                .findJwtUsersByUsername(username)
+                .orElseThrow(() -> new EntityNotFoundException("User Not found by username " + username));
     }
 
 }
