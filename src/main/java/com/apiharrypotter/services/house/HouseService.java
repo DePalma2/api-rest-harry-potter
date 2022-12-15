220d02425f3d60003419bbe46bccc55361743554
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-12-15 19:54:06 -0300

    fix(getById): Fix Optional method
    Add Optional in getById method

--- src/main/java/com/apiharrypotter/services/house/HouseService.java
+++ src/main/java/com/apiharrypotter/services/house/HouseService.java
@@ -3,11 +3,12 @@ package com.apiharrypotter.services.house;
 import com.apiharrypotter.entities.House;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface HouseService {
 
     public List<House> getAllHouses();
-    public House getById(Long id);
+    public Optional<House> getById(Long id);
     public House addHouse(House house);
     public House updateHouse(Long id,House house);
     public boolean deleteHouse(House house);
