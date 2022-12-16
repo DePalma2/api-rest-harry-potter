41d1158299b4cd3d68ddde7b93b66a0bec6f4a15
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-12-16 14:59:36 -0300

    ChapterController added

---
+++ src/main/java/com/apiharrypotter/controller/chapter/ChapterController.java
@@ -0,0 +1,43 @@
+package com.apiharrypotter.controller.chapter;
+
+import com.apiharrypotter.entities.Chapter;
+import com.apiharrypotter.services.chapter.ChapterServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ChapterController {
+
+    @Autowired
+    private ChapterServiceImpl chapterService;
+
+    @GetMapping("/chapters")
+    public List<Chapter> getAllChapter(){
+        return chapterService.getAllChapter();
+    }
+
+    @GetMapping( "/chapters/{id}")
+    public Optional<Chapter> getChapterById(@PathVariable(name = "id") Long id) {
+        return chapterService.getChapterById(id);
+    }
+
+    @PostMapping("/chapters")
+    public Chapter addChapter(@RequestBody Chapter chapter) {
+        return chapterService.addChapter(chapter);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteChapter(@PathVariable(name = "id") Chapter id) {
+        boolean ok = chapterService.removeChapter(id);
+        if(ok){
+            return "Chapter remove with id:  " + id;
+        }else{
+            return "Error try remove chapter with id: " + id;
+        }
+    }
+
+}
