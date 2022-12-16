ccef82cbe97e45908b0890519dbe46adabe952e3
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-12-16 15:55:11 -0300

    Fix getAllPotions method

--- src/main/java/com/apiharrypotter/services/potion/PotionService.java
+++ src/main/java/com/apiharrypotter/services/potion/PotionService.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public interface PotionService {
 
-    public List<Potion> getAllPotions(Potion potion);
+    public List<Potion> getAllPotions();
     public Optional<Potion> getById(Long id);
     public Potion addPotion(Potion potion);
     public Potion updatePotion(Long id, Potion potion);
