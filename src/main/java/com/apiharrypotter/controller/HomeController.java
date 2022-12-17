c1630f1a40212b5380593d51a27944f64e7026b4
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-12-17 20:20:58 -0300

    Change redirect path "/"

--- src/main/java/com/apiharrypotter/controller/HomeController.java
+++ src/main/java/com/apiharrypotter/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.apiharrypotter.controller;
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,16 +10,18 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping(name = "/api/v1")
+@Api(tags = "Welcome Harry Potter API", value = "Home", description = "These are the information and redirection endpointss")
 public class HomeController {
 
     @GetMapping(name = "/")
     public void welcomeHome(HttpServletResponse response) throws IOException {
-       response.sendRedirect("/home");
+       response.sendRedirect("/swagger-ui.html");
     }
 
     @GetMapping("/home")
     public String welcomeHome() {
-        return "Welcome to the Harry Potter API";
+        return "Welcome to the Harry Potter API, \n" +
+                "The home page is under construction ";
     }
 
 
