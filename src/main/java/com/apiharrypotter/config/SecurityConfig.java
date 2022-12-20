3a547c19f929ff16e992db51cd58ca59fae8c411
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-12-19 23:18:32 -0300

    Secure Endpoints

---
+++ src/main/java/com/apiharrypotter/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.apiharrypotter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/add/**").authenticated()
+                .antMatchers("/delete/**").authenticated()
+                .anyRequest().permitAll().and().formLogin();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**");
+    }
+}
