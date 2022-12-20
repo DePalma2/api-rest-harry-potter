97462a82d5017a752428004b0193f6470d0498f9
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-12-19 21:49:15 -0300

    Spells endpoint documentation, details aggregation and response

--- src/main/java/com/apiharrypotter/controller/spell/SpellController.java
+++ src/main/java/com/apiharrypotter/controller/spell/SpellController.java
@@ -2,7 +2,10 @@ package com.apiharrypotter.controller.spell;
 
 import com.apiharrypotter.entities.Spell;
 import com.apiharrypotter.services.spell.SpellServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,28 +13,37 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
+@Api(tags = "Spells", value = "Spell", description = "Information about the spells")
 public class SpellController {
 
     @Autowired
     private SpellServiceImpl spellService;
 
     @GetMapping("/spells")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "List all spells")
     public List<Spell> getAllSpells() {
         return spellService.getAllSpells();
     }
 
     @GetMapping("/spells/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Returns a spell with a specific id", notes = "Spell must exist")
     public Optional<Spell> getSpellsById(@PathVariable(name = "id") Long id) {
         return spellService.getById(id);
     }
 
-    @PostMapping("/spells/add")
+    @PostMapping("/add/spells")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value = "Create a spell")
     public Spell addSpell(@RequestBody Spell spell) {
         spellService.addSpell(spell);
         return spell;
     }
 
-    @DeleteMapping("/spells/{id}/delete")
+    @DeleteMapping("/delete/spells/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Delete a spell", notes = "Spell must exist")
     public String deleteSpell(@PathVariable(name = "id") Spell id) {
         boolean ok = spellService.deleteSpell(id);
         if(ok){
