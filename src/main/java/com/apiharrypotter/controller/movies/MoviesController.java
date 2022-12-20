27d292f975dcf584059c080e2dcf91f2c8078b31
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-12-19 21:48:52 -0300

    Movies endpoint documentation, details aggregation and response

--- src/main/java/com/apiharrypotter/controller/movies/MoviesController.java
+++ src/main/java/com/apiharrypotter/controller/movies/MoviesController.java
@@ -2,7 +2,10 @@ package com.apiharrypotter.controller.movies;
 
 import com.apiharrypotter.entities.Movies;
 import com.apiharrypotter.services.movies.MovieServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,28 +13,37 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
+@Api(tags = "Movies", value = "Movie", description = "Information about the movies")
 public class MoviesController {
 
     @Autowired
     private MovieServiceImpl movieService;
 
     @GetMapping("/movies")
+    @ApiOperation(value = "List all movies")
+    @ResponseStatus(HttpStatus.OK)
     public List<Movies> getAllMovies() {
         return movieService.getAllMovies();
     }
 
     @GetMapping("/movies/{id}")
+    @ApiOperation(value = "Returns a movie with a specific id", notes = "Movie must exist")
+    @ResponseStatus(HttpStatus.OK)
     public Optional<Movies> getMoviesById(@PathVariable(name = "id") Long id){
         return movieService.getById(id);
     }
 
-    @PostMapping("/movies/add")
+    @PostMapping("/add/movies")
+    @ApiOperation(value = "Create a movie")
+    @ResponseStatus(HttpStatus.CREATED)
     public Movies addMovies(@RequestBody Movies movies) {
         movieService.addMovies(movies);
         return movies;
     }
 
-    @DeleteMapping("/movies/{id}/delete")
+    @DeleteMapping("/delete/movies/{id}")
+    @ApiOperation(value = "Delete a chapter", notes = "Movie must exist")
+    @ResponseStatus(HttpStatus.OK)
     public String deleteMoviesById(@PathVariable(name = "id") Movies id) {
         boolean ok = movieService.deleteMovie(id);
         if(ok){
