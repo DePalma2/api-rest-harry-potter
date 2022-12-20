876f049f05876ddd21a9f89028b5cdb23c07f4fd
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-12-19 21:48:43 -0300

    House endpoint documentation, details aggregation and response

--- src/main/java/com/apiharrypotter/controller/house/HouseController.java
+++ src/main/java/com/apiharrypotter/controller/house/HouseController.java
@@ -2,7 +2,10 @@ package com.apiharrypotter.controller.house;
 
 import com.apiharrypotter.entities.House;
 import com.apiharrypotter.services.house.HouseServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,28 +13,37 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
+@Api(tags = "Houses", value = "House", description = "Information about the houses")
 public class HouseController {
 
     @Autowired
     private HouseServiceImpl houseService;
 
     @GetMapping("/houses")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "List all houses")
     public List<House>  getAllHouses(){
         return houseService.getAllHouses();
     }
 
     @GetMapping("/houses/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Returns a house with a specific id", notes = "House must exist")
     public Optional<House> getHouseById(@PathVariable(name = "id") Long id){
         return houseService.getById(id);
     }
 
-    @PostMapping("/houses/add")
+    @PostMapping("/add/houses")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value = "Create a house")
     public House addHouse(@RequestBody  House house){
         houseService.addHouse(house);
         return house;
     }
 
-    @DeleteMapping("/houses/{id}/delete")
+    @DeleteMapping("/delete/houses/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Delete a house")
     public String deleteHouse(@PathVariable(name = "id") House id){
         boolean ok = houseService.deleteHouse(id);
         if(ok){
